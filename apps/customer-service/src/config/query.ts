02a0f07c9df4aaa801e4ff632d83112ed1d9f934
nrv23 <navemen23@hotmail.com>
2026-07-06 01:44:40 -0600

    cambiar la implementacion de la clase de Database

--- apps/customer-service/src/config/query.ts
+++ apps/customer-service/src/config/query.ts
@@ -3,25 +3,37 @@
 // Also exports a singleton instance (`databaseInstance`) that can be shared
 // across the application to avoid creating multiple wrapper objects.
 
+
 import type { QueryResultRow } from "pg";
 import { pool } from "./db.js";
 import { IDatabase } from "../interfaces/database.interface.js";
 
 export class Database implements IDatabase {
-    /**
-     * Ejecuta una consulta tipada contra PostgreSQL usando el pool
-     * compartido. El pool mantiene sus propias conexiones y se re‑usa
-     * internamente, por lo que no hay sobrecarga de crear nuevas
-     * conexiones en cada request.
-     */
     async query<T extends QueryResultRow>(
         text: string,
         params?: unknown[],
     ): Promise<T[]> {
-        const result = await pool.query<T>(text, params);
-        return result.rows;
+        const client = await pool.connect();
+        console.log("Nueva conexion....");
+        console.log({
+            client: typeof client
+        })
+        /**
+         * Ejecuta una consulta tipada contra PostgreSQL usando el pool
+         * compartido. El pool mantiene sus propias conexiones y se re‑usa
+         * internamente, por lo que no hay sobrecarga de crear nuevas
+         * conexiones en cada request.
+         */
+        try {
+            const result = await client.query<T>(text, params);
+            return result.rows;
+        } finally {
+            client.release();
+            console.log("Conexion liberada")
+        }
     }
 }
+
 
 // Instancia única que pueden usar los repositorios que no inyecten su propio DB.
 export const databaseInstance = new Database();
