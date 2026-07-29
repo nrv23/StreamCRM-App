0bc5691c888241d29197269dc461c2317744395f
nrv23 <navemen23@hotmail.com>
2026-07-28 21:24:18 -0600

    agregar valores especificos a createTagHandler

--- apps/notification-service/src/handlers/createTag.handler.ts
+++ apps/notification-service/src/handlers/createTag.handler.ts
@@ -1,6 +1,8 @@
 import { CreateNotificationDto } from "../dto/notifications/create-notification.dto.ts";
 import { IntegrationEventHandler } from "../interfaces/handler/integration-event-handler.interface.ts";
 import { INotificationRepository } from "../interfaces/notification/notification-repository.interface.ts";
+import { NotificationStatus } from "../shared/types/notification-status.type.ts";
+import { NotificationType } from "../shared/types/notification-type.type.ts";
 
 
 export class CreateTagHandler implements IntegrationEventHandler {
@@ -11,6 +13,10 @@ export class CreateTagHandler implements IntegrationEventHandler {
     }
     async handle(event: CreateNotificationDto): Promise<void> {
 
+        event.title = 'New Tag created';
+        event.message = `Tag created with name ${event.metadata.tag_name}`
+        event.type = NotificationType.INFO;
+        event.status = NotificationStatus.PENDING;
         await this._notificationRepository.save(event)
     }
 }
